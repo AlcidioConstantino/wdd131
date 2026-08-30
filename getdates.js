7777ecf63f94096643a468cf66ac0ad060e7ce55
AlcidioConstantino <amassanganhe333@gmail.com>
2026-08-30 15:19:47 +0200

    Complete WDD 131 Week 1 homepage

---
+++ getdates.js
@@ -0,0 +1,10 @@
+```javascript
+const currentYear = new Date().getFullYear();
+
+document.querySelector("#currentyear").textContent = currentYear;
+
+const lastModified = document.lastModified;
+
+document.querySelector("#lastModified").textContent =
+    `Last Modified: ${lastModified}`;
+```
